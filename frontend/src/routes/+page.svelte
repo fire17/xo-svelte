239e47d540613a4de1f9a4e2d1941a47beb5db2b
Tami Bar <fire17@gmail.com>
2023-07-20 18:58:36 +0300

    trying out grid

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -142,8 +142,91 @@ export function sendSocketIOMessage(msg) {
 
 
 
+	import Grid from "svelte-grid";
+	import gridHelp from "svelte-grid/build/helper/index.mjs";
+// import Grid from "../../components/svelte-grid/index.svelte";
+// import gridHelp from "../../components/svelte-grid/utils/helper.js";
+// import { openSource } from "./github.js";
 
+const COLS = 6;
 
+const id = () => "_" + Math.random().toString(36).substr(2, 9);
+
+const randomNumberInRange = (min, max) => Math.random() * (max - min) + min;
+
+let items = [
+  {
+    [COLS]: gridHelp.item({
+      x: 0,
+      y: 0,
+      w: 2,
+      h: 2,
+    }),
+    id: id(),
+  },
+
+  {
+    [COLS]: gridHelp.item({
+      x: 2,
+      y: 0,
+      w: 2,
+      h: 2,
+    }),
+    id: id(),
+  },
+];
+
+const cols = [[1100, 6]];
+
+function add() {
+  let newItem = {
+    6: gridHelp.item({
+      w: Math.round(randomNumberInRange(1, 4)),
+      h: Math.round(randomNumberInRange(1, 4)),
+      x: 0,
+      y: 0,
+    }),
+    id: id(),
+  };
+
+  let findOutPosition = gridHelp.findSpace(newItem, items, COLS);
+
+  newItem = {
+    ...newItem,
+    [COLS]: {
+      ...newItem[COLS],
+      ...findOutPosition,
+    },
+  };
+
+  items = [...items, ...[newItem]];
+}
+
+const addAt = () => {
+  let newItem = {
+    6: gridHelp.item({
+      w: Math.round(randomNumberInRange(1, 4)),
+      h: Math.round(randomNumberInRange(1, 4)),
+      x: 0,
+      y: 0,
+    }),
+    id: id(),
+  };
+
+  items = [...[newItem], ...items];
+
+  items = gridHelp.adjust(items, COLS);
+};
+
+const remove = (item) => {
+  items = items.filter((value) => value.id !== item.id);
+
+  if (adjustAfterRemove) {
+    items = gridHelp.adjust(items, COLS);
+  }
+};
+
+let adjustAfterRemove = false;
 </script>
 
 <svelte:head>
@@ -152,7 +235,56 @@ export function sendSocketIOMessage(msg) {
 </svelte:head>
 
 <section>
-	<h1>
+
+
+
+
+
+
+
+<button on:click={add}>Add (random size)</button>
+<button on:click={addAt}>Add random (x=0,y=0)</button>
+<label>
+  <input type="checkbox" bind:checked={adjustAfterRemove} />
+  Adjust elements after removing an item
+</label>
+
+<div class=demo-container>
+  <Grid bind:items={items} rowHeight={100} let:item let:dataItem {cols}>
+    <div class=demo-widget>
+      <span on:pointerdown={e => e.stopPropagation()}
+        on:click={() => remove(dataItem)}
+        class=remove
+        >
+        ✕
+      </span>
+      <p>{dataItem.id}</p>
+    </div>
+  </Grid>
+</div>
+
+
+
+
+
+
+
+
+
+
+	<!-- <Grid {cols} bind:items={items} let:item={item}>
+	  <div>{item.id}</div>
+</Grid> -->
+	
+<!-- <Grid gap={[10, 5]} {items} {cols} rowHeight={150} let:item let:dataItem>
+  <div>{item.data}</div>
+</Grid> -->
+
+
+
+
+
+<h1>
 		<!-- <span class="welcome">
 			<picture>
 				<source srcset={welcome} type="image/webp" />
@@ -213,6 +345,8 @@ export function sendSocketIOMessage(msg) {
 
 
 <style>
+	.remove { cursor: pointer; position: absolute; right: 5px; top: 3px; }
+
 	section {
 		display: flex;
 		flex-direction: column;
